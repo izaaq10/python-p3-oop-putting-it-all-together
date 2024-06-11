5ffc4a0c502167071f19117cbd0131e316bb934b
Izaaq <odongoizaaq@gmail.com>
2024-06-11 02:00:55 -0400

    commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,14 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
     
-        
+    def set_page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+            self.page_count = None
+        else:
+            self.page_count = page_count
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New" 
+    
+    def set_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer" )
+            self.size = None
+        else:
+            self.size = size
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
